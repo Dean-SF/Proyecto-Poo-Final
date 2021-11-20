bdf086aad5e383e1e6b0ec2f7e94473e012cc20e
EstebanPP <81255764+EstebanPP@users.noreply.github.com>
2021-11-20 15:35:38 -0600

    producto y builder
    falta ver lo de la imagen

--- codigo/src/datos/Producto.java
+++ codigo/src/datos/Producto.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package datos;
 
 /**
@@ -9,5 +5,88 @@ package datos;
  * @author DeanSF
  */
 public class Producto {
-    
+   private String codigo;
+   private String nombre;
+   private String descripcion;
+   private int size;
+   private int cantidad;
+   private int calorias;
+   private int caloriasUnidad;
+   private int precio;
+   
+   public Producto(){};
+   public Producto(String codigo, String nombre, String desccripcion, int size, int cantidad, int calorias, int caloriasUnidad, int precio) {
+       this.codigo = codigo;
+       this.nombre = nombre;
+       this.descripcion = desccripcion;
+       this.size = size;
+       this.cantidad = cantidad;
+       this.calorias = calorias;
+       this.caloriasUnidad = caloriasUnidad;
+       this.precio = precio;
+   }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setCalorias(int calorias) {
+        this.calorias = calorias;
+    }
+
+    public void setCaloriasUnidad(int caloriasUnidad) {
+        this.caloriasUnidad = caloriasUnidad;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public int getCalorias() {
+        return calorias;
+    }
+
+    public int getCaloriasUnidad() {
+        return caloriasUnidad;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
 }
---
+++ codigo/src/datos/ProductoBuilder.java
@@ -0,0 +1,53 @@
+package datos;
+
+/**
+ *
+ * @author Esteban
+ */
+public class ProductoBuilder {
+    private String codigo;
+   private String nombre;
+   private String descripcion;
+   private int size;
+   private int cantidad;
+   private int calorias;
+   private int caloriasUnidad;
+   private int precio;
+   
+   public ProductoBuilder(){};
+    public Producto buildProducto(){
+        return new Producto(codigo,nombre,descripcion,size,cantidad,calorias,caloriasUnidad,precio);
+    }
+    public ProductoBuilder codigo(String codigo){
+        this.codigo = codigo;
+        return this;
+    }
+    public ProductoBuilder nombre(String nombre){
+        this.nombre = nombre;
+        return this;
+    }
+    public ProductoBuilder descripcion(String descripcion){
+        this.descripcion = descripcion;
+        return this;
+    }
+    public ProductoBuilder size(int size){
+        this.size = size;
+        return this;
+    }
+    public ProductoBuilder cantidad(int cantidad){
+        this.cantidad = cantidad;
+        return this;
+    }
+    public ProductoBuilder calorias(int calorias){
+        this.calorias = calorias;
+        return this;
+    }
+    public ProductoBuilder caloriasUnidad(int caloriasUnidad){
+        this.caloriasUnidad = caloriasUnidad;
+        return this;
+    }
+    public ProductoBuilder precio(int precio){
+        this.precio = precio;
+        return this;
+    }
+}
